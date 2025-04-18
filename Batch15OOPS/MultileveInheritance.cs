bf89e1e5ce693c8f02af89d3c084e01c07f8788f
Dhananjay Masal <dhananjay9096@gmail.com>
2025-04-18 08:50:20 +0530

    aassignent

---
+++ Batch15OOPS/MultileveInheritance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPS
+{
+
+
+
+    class A // base class  -- grand parent
+    {
+        public void start()
+        {
+            Console.WriteLine("The vehical is started");
+
+        }
+
+
+    }
+    class B : A //derived -- base class   - parent 
+    {
+
+        public void Drive()
+        {
+            Console.WriteLine("car is driving");
+
+        }
+
+    }
+
+    class C : B   //derived class  - chield
+    {
+
+        public void Troubleshoot()
+        {
+            Console.WriteLine(" the sport is in the another mode");
+        }
+    }
+
+    internal class MultileveInheritance
+    {
+        static void Main()
+        {
+
+            C cls = new C();
+            cls.start();
+            cls.Drive();
+            cls.Troubleshoot();
+
+
+        }
+
+    }
+}
---
+++ Batch15OOPS/SingleInheritance.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch15OOPS
+{
+
+
+    class Vehical  //Base class or parent class or superclass
+    {
+        public string Brand { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{Brand} is driving");
+        }
+
+    }
+
+    class Car : Vehical // derived class or cheild or sub class 
+    {
+        public int NumberofDoor { get; set; }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"{Brand} car has {NumberofDoor} door");
+        }
+
+    }
+
+
+    internal class SingleInheritance
+    {
+        static void Main()
+        {
+
+            Car mycar = new Car();
+            mycar.Brand = "Hundai";
+            mycar.NumberofDoor =4;
+            mycar.Drive();
+            mycar.ShowCarInfo();
+
+        }
+
+
+
+    }
+}
